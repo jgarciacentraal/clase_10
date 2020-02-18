763d8c427a8540b9624b102ab9a7f3cfa6374372
Julio Estefanes <jsgr1992@gmail.com>
2020-02-17 22:14:42 -0600

    No subi al repo el dia de la clase

---
+++ custom-math/index.js
@@ -0,0 +1,10 @@
+const suma = require('./suma')
+//const resta =  require('./resta')
+//const multiplicacion = require('./multiplicacion')
+//const division = require('./division')
+
+
+module.exports.suma = suma
+//module.exports.resta = resta
+//module.exports.multiplicacion = multiplicacion
+//module.exports.division = division
---
+++ sample-math.js
@@ -0,0 +1,6 @@
+var customMath = require('./custom-math')
+
+console.log(customMath.suma(10,5));
+//console.log(customMath.resta(10,2))
+//console.log(customMath.division(10,5))
+//console.log(customMath.multiplicacion(2,3))
